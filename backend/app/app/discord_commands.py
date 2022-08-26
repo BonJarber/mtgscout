4ee7e39e04a34722a04c0a5a8e75b320188e0d7f
bonjarber <jonbarberemail@gmail.com>
2022-08-26 01:17:35 -0700

    Add initial discord support for parsing ck URL

--- backend/app/app/discord_commands.py
+++ backend/app/app/discord_commands.py
@@ -1,7 +1,10 @@
 import discord
+import validators
+from urllib.parse import urlparse
 from discord.ext import commands
 
 from app.core.config import settings
+from app.stores.card_kingdom import parse_cardkingdom_url
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,8 +17,16 @@ async def on_ready():
 
 
 @bot.command()
-async def scout(ctx: commands.Context, input: str):
-    await ctx.send(f"Querying {input} for data")
+async def scout(ctx: commands.Context, input_sr: str):
+    if validators.url(input_sr):
+        url = urlparse(input_sr)
+        if not url:
+            await ctx.send(
+                f"Could not properly parse the URL, double check and try again"
+            )
+        if url.netloc == "www.cardkingdom.com":
+            result = parse_cardkingdom_url(url)
+            await ctx.send(result)
 
 
 async def run_bot():
