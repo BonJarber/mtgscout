ed58333f4c6eeda5add86b65ed572d20afe0eeb8
bonjarber <jonbarberemail@gmail.com>
2022-08-27 16:10:31 -0700

    Add set data object, handle lookuping up new cards, and add set_name <-> set_code mapping functionality

--- backend/app/app/stores/card_kingdom.py
+++ backend/app/app/stores/card_kingdom.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import ParseResult
 
 from app.db.session import SessionLocal
+from app.tasks import lookup_card
 from app import crud
 
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
@@ -16,7 +17,7 @@ def parse_cardkingdom_url(url: ParseResult) -> str:
         return "Sorry I only support MTG cards at this time"
 
     html_text = requests.get(url.geturl(), headers={"user-agent": USER_AGENT}).text
-    soup = BeautifulSoup(html_text)
+    soup = BeautifulSoup(html_text, features="lxml")
 
     conditions = soup.find("ul", {"class": "cardTypeList"}).find_all("li")
     if len(conditions) == 0:
@@ -24,6 +25,8 @@ def parse_cardkingdom_url(url: ParseResult) -> str:
 
     default_condition = None
     for condition in conditions:
+        if len(condition["class"]) < 2:
+            continue
         if condition["class"][1] == "active":
             default_condition = condition["class"][0]
 
@@ -34,19 +37,11 @@ def parse_cardkingdom_url(url: ParseResult) -> str:
     if card_data.find("input", {"name": "model"})["value"] == "mtg_foil":
         is_foil = True
 
-    db = SessionLocal()
-    potential_cards = crud.card.get_by_name_set(
-        db=db, name=card_name, set_name=card_set
-    )
-    if len(potential_cards) > 1:
-        message = "I found multiple possible cards:\n"
-        for card in potential_cards:
-            message += f"{card.name} - {card.set_name} #{card.scryfall_id}"
-        return message
-    elif len(potential_cards) == 0:
-        return f"I couldn't find the card {card_name} from {card_set}"
-    card = potential_cards[0]
-    message = "I found the card!"
-    message += f"{card.name} - {card.set_name} #{card.scryfall_id}"
+    card = lookup_card(name=card_name, set_name=card_set)
+    if not card:
+        message = "I failed to look up the card"
+    else:
+        message = "I found the card!"
+        message += f"\n{card.name} from {card.set_name} \n#{card.scryfall_id}"
     return message
 
--- backend/app/app/tasks.py
+++ backend/app/app/tasks.py
@@ -1,6 +1,9 @@
+import json
 import logging
+import requests
 
 from app.db.session import SessionLocal
+from app.schemas.card import CardCreate, Card
 from app import crud
 
 logging.basicConfig(level=logging.INFO)
@@ -35,3 +38,72 @@ def check_scouts() -> None:
 
     logger.info(f"Finished, checked {count} scouts")
     return
+
+
+def lookup_set_code(set_name: str) -> str:
+    with open("app/assets/set_data.json") as f:
+        sets = json.load(f)
+    for s in sets:
+        if s["name"] == set_name:
+            return s["code"]
+    return None
+
+
+def lookup_card(name: str, set_name: str) -> Card:
+    logging_msg = f"Attempting to lookup [[{name}]]"
+    if set_name:
+        logging_msg += f" from {set_name}"
+    logger.info(logging_msg)
+    # Load DB connection
+    db = init_db()
+
+    # Check if card already exists in the DB
+    cards = crud.card.get_by_name_set(db=db, name=name, set_name=set_name)
+    if len(cards) == 1:
+        return cards[0]
+
+    # Not in DB as is, look up the card using fuzzy search from scryfall
+    SCRYFALL_FUZZY_SEARCH_ENDPOINT = "https://api.scryfall.com/cards/named?fuzzy="
+    if name == "" or name is None:
+        logger.info("No card name was provided to look up")
+        return None
+
+    query = SCRYFALL_FUZZY_SEARCH_ENDPOINT + name
+    if set_name:
+        if len(set_name) > 5:
+            set_code = lookup_set_code(set_name)
+            if set_code is None:
+                logger.info(f"Could not find set code for {set_name}")
+            query += f"&set={set_code}"
+        else:
+            query += f"&set={set_name}"
+    card_data = requests.get(query).json()
+    if card_data["object"] == "error":
+        logger.info(f"Error looking up card from scryfall: {card_data['details']}")
+        return None
+    elif card_data["object"] != "card":
+        logger.info("Something went wrong looking up the card on scryfall")
+        return None
+
+    # Check to see if the proper card/set name exists in DB
+    cards = crud.card.get_by_name_set(
+        db=db, name=card_data["name"], set_name=card_data["set_name"]
+    )
+    if len(cards) == 1:
+        return cards[0]
+
+    # This means it's definitely not in DB, so add it
+
+    # Change values to match DB
+    card_data["scryfall_id"] = card_data["id"]
+    card_data["set_shortname"] = card_data["set"]
+    card_data.pop("id", None)
+    card_data.pop("set", None)
+
+    card_in = CardCreate(**card_data)
+    card = crud.card.create(db=db, obj_in=card_in)
+    if not card:
+        logger.info("Something went wrong creating the card object from scryfall")
+        return None
+
+    return card
